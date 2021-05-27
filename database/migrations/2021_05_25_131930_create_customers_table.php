b0308c7351bc96c3a26d3492e49b8cdba3f56c12
Chris Deuda <christopherdeuda@gmail.com>
2021-05-28 00:47:15 +0800

    Add migration table

---
+++ database/migrations/2021_05_25_131930_create_customers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 300);
+            $table->string('last_name', 300);
+            $table->string('email', 300);
+            $table->string('username', 300);
+            $table->string('password', 300);
+            $table->string('gender', 10);
+            $table->string('country',300);
+            $table->string('city');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
